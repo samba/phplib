e093e47bdbd872e5caebb392e0b609fd998dc49f
Sam Briesemeister <sam@analyticspros.com>
2012-03-12 23:21:14 -0700

    Transition init to loading modules by dedicated constants

--- lib/init.php
+++ lib/init.php
@@ -1,5 +1,7 @@
 <?php
 
+# Setup defaults and load configured libraries
+
 # Where is this library loading?
 define('LIB_DIR', dirname(__FILE__));
 
@@ -7,7 +9,9 @@ define('LIB_DIR', dirname(__FILE__));
 defined('PROJECT_DIR') || define('PROJECT_DIR', '../');
 
 # Should we load HTTP-method handlers automatically?
-defined('AUTO_METHOD') || define('AUTO_METHOD', false);
+defined('AUTO_BUFFER_OUTPUT') || define('AUTO_BUFFER_OUTPUT', true);
+defined('LOAD_REQUEST_HANDLERS') || define('LOAD_REQUEST_HANDLERS', true);
+defined('LOAD_RESPONSE_HANDLERS') || define('LOAD_RESPONSE_HANDLERS', true);
 
 # Environment defaults for URL parsers
 defined('REQUEST_METHOD') || define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
@@ -22,12 +26,20 @@ defined('REQUEST_PATH') || define('REQUEST_PATH', str_replace($_SERVER['QUERY_ST
 
 require_once(constant('LIB_DIR') . '/util.php');
 
-# Automation classes for HTTP methods
-if(constant('AUTO_METHOD')){
-  ob_start();
+# Buffer output from the start?
+if(constant('AUTO_BUFFER_OUTPUT')) ob_start();
+
+# Load request handler automation class (map HTTP method to class method)
+if(constant('LOAD_REQUEST_HANDLERS'))
   require_once(constant('LIB_DIR') . '/request.php');
+
+# Load response handler automation
+if(constant('LOAD_RESPONSE_HANDLERS'))
   require_once(constant('LIB_DIR') . '/response.php');
-}
+
+# Load data model abstractions
+if(constant('LOAD_MODELS'))
+  require_once(constant('LIB_DIR') . '/models.php');
 
 # URL mapping automation
 require_once(constant('LIB_DIR') . '/urlmap.php');
